1bc00679a72ad10904e785ae1c17b85ebfee0599
Ziad Mallah <ziad@elmallah.com>
2016-10-22 15:42:22 -0400

    renamed tornyapi to torny_backend, furthermore, I changed our backend to use mariadb instead of postgresql. made further steps in using models

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import pymysql
+pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tornyapi.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "torny_backend.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
