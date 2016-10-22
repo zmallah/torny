d85969b23fbba278dca803ec1c94661a0b6fc3a0
Ziad Mallah <ziad@elmallah.com>
2016-10-22 14:29:33 -0400

    Configured docker, and started structuring tables

--- tornyapi/settings.py
+++ tornyapi/settings.py
@@ -74,6 +74,16 @@ WSGI_APPLICATION = 'tornyapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
